bd835af2c70684f23a158577ad0106d96aee531b
jzscu <961564853@qq.com>
2018-08-16 15:15:27 +0800

    update

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ import utils
 import time
 import os
 
-from model import C3D, MutiGPU
+from model import C3D, MultiGPU, get_multi_gpu_models
 import tensorflow as tf
 
 def valid(valid_data, model, sess):
@@ -20,29 +20,26 @@ def valid(valid_data, model, sess):
   print "auc %f in valid comments" % auc
 
 def main(args):
-  save_dir = os.path.join(args.save_dir, args.model_type)
-  log_dir = os.path.join(args.log_dir, args.model_type)
+  save_dir = os.path.join(args.save_dir)
+  log_dir = os.path.join(args.log_dir)
 
   if not os.path.exists(save_dir):
     os.makedirs(save_dir)
   if not os.path.exists(log_dir):
     os.makedirs(log_dir)
-  if not os.path.exists(img_dir):
-    os.makedirs(img_dir)
 
   summary_writer = tf.summary.FileWriter(log_dir)
   config_proto = utils.get_config_proto()
 
   sess = tf.Session(config=config_proto)
-  model = C3D(args, name="c3d")
-  models = utils.get_multi_gpu_models(args, model)
-  trainer = MultiGPU(args, sess, models)
+  models = get_multi_gpu_models(args, sess)
+  trainer = MultiGPU(args, models)
 
   for step in range(1, args.nb_steps+1):
     step_start_time = time.time()
-    train_images, train_labels, _, _, _ = input_data.read_clip_and_label(filename='list/train.list',
-          batch_size=args.batch_size * args.agpu_num, num_frames_per_clip=c3d_model.NUM_FRAMES_PER_CLIP,
-          crop_size=c3d_model.CROP_SIZE, shuffle=True)
+    train_images, train_labels, _, _, _ = utils.read_clip_and_label(filename='list/train.list',
+          batch_size=args.batch_size * args.num_gpu, num_frames_per_clip=args.frame_size,
+          crop_size=args.img_h, shuffle=True)
     _, loss, summaries = trainer.train(sess, train_images, train_labels)
     writer.add_summary(summary, step)
 
