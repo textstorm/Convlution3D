b835276eecf9b8ea2cbb1fdbf0e8c5192a769fd1
textstorm <jiangzhewise@gmail.com>
2018-08-30 21:34:34 +0800

    update

--- model.py
+++ model.py
@@ -48,32 +48,33 @@ class C3D(object):
       cross_entropy = tf.reduce_mean(tf.nn.sparse_softmax_cross_entropy_with_logits(
                                     labels=self.labels, logits=logits))
       tf.summary.scalar("loss" + '_cross_entropy', cross_entropy)
-      weight_decay_loss = tf.reduce_mean(tf.get_collection('weightdecay_losses', scope="model_%d"%idx))
-      tf.summary.scalar("loss" + '_weight_decay_loss', tf.reduce_mean(weight_decay_loss))
-      self.total_loss = cross_entropy + weight_decay_loss
-      tf.summary.scalar("loss" + '_total_loss', tf.reduce_mean(self.total_loss))
+      self.total_loss = cross_entropy
+      # weight_decay_loss = tf.reduce_mean(tf.get_collection('weightdecay_losses', scope="model_%d"%idx))
+      # tf.summary.scalar("loss" + '_weight_decay_loss', tf.reduce_mean(weight_decay_loss))
+      # self.total_loss = cross_entropy + weight_decay_loss
+      # tf.summary.scalar("loss" + '_total_loss', tf.reduce_mean(self.total_loss))
 
     with tf.name_scope("inference"):
       self.infer_op = tf.argmax(logits, 1)
 
     with tf.name_scope("accuracy"):
       correct_pred = tf.equal(tf.argmax(logits, 1), self.labels)
-      accuracy = tf.reduce_mean(tf.cast(correct_pred, tf.float32))
+      self.accuracy = tf.reduce_mean(tf.cast(correct_pred, tf.float32))
 
     self.tvars = tf.trainable_variables()
-    init_op = tf.global_variables_initializer()
-    self.sess.run(init_op)
     self.summary = tf.summary.merge_all()
     self.saver = tf.train.Saver(tf.global_variables())
 
   def conv3d(self, x, fs, hs, ws, n_in, n_out, name=None):
-    w = self.variable_with_weight_decay(shape=[fs, hs, ws, n_in, n_out], name=name+"_w", wd=0.0005)
+    # w = self.variable_with_weight_decay(shape=[fs, hs, ws, n_in, n_out], name=name+"_w", wd=0.0005)
+    w = self.variable_with_weight_decay(shape=[fs, hs, ws, n_in, n_out], name=name+"_w")
     b = self.variable_with_weight_decay(shape=[n_out], name=name+"_bias")
     x = tf.nn.conv3d(x, w, strides=[1, 1, 1, 1, 1], padding='SAME', name=name)
     return tf.nn.bias_add(x, b)
 
   def linear(self, x, units, name=None):
-    w = self.variable_with_weight_decay(shape=[x.get_shape().as_list()[-1], units], name=name+"_w", wd=0.0005)
+    # w = self.variable_with_weight_decay(shape=[x.get_shape().as_list()[-1], units], name=name+"_w", wd=0.0005)
+    w = self.variable_with_weight_decay(shape=[x.get_shape().as_list()[-1], units], name=name+"_w")
     b = self.variable_with_weight_decay(shape=[units], name=name+"_bias")
     return tf.nn.bias_add(tf.matmul(x, w), b)
 
@@ -95,20 +96,24 @@ class C3D(object):
       tf.add_to_collection('weightdecay_losses', weight_decay)
     return var
 
-def get_multi_gpu_models(args, sess, restore=False):
+def get_multi_gpu_models(args, sess):
   models = []
   with tf.variable_scope(tf.get_variable_scope()):
     for gpu_idx in range(args.num_gpu):
       with tf.name_scope("model_%d"%gpu_idx), tf.device("/gpu:%d"%gpu_idx):
         c3d = C3D(args, sess, gpu_idx, name="C3D")
-        if restore:
-          restore_dict = restore_func(args.load_path, c3d.tvars)
-          c3d.saver = tf.train.Saver(restore_dict)
-          sess.run(tf.global_variables_initializer())
-          c3d.saver.restore(sess, args.load_path)
         tf.get_variable_scope().reuse_variables()
         models.append(c3d)
   return models
+
+def restore_models(args, sess, models):
+  new_models = []
+  for model in models:
+    restore_dict = restore_func(args.load_path, model.tvars)
+    model.saver = tf.train.Saver(restore_dict)
+    model.saver.restore(sess, args.load_path)
+    new_models.append(model)
+  return new_models
 
 def restore_func(load_path, tvars):
   reader = tf.train.NewCheckpointReader(load_path)
@@ -139,7 +144,6 @@ def restore_func(load_path, tvars):
       name += "_w"
     elif "wout" in name:
       name = name.replace("wout", "logits_w")
-
     else:
       pass
     convert_name.append(name)
@@ -149,10 +153,14 @@ def restore_func(load_path, tvars):
   for v in tvars:
     tensor_name = v.name.split(':')[0]
     if reader.has_tensor(convert_dict[tensor_name]):
-      print('has tensor ', tensor_name)
+      # print('has tensor ', tensor_name)
       restore_dict[convert_dict[tensor_name]] = v
   restore_dict.pop("var_name/wout")
   restore_dict.pop("var_name/bout")
+  # restore_dict.pop('var_name/bd1')
+  # restore_dict.pop('var_name/bd2')
+  # restore_dict.pop('var_name/wd1')
+  # restore_dict.pop('var_name/wd2')
   return restore_dict
 
 class MultiGPU(object):
@@ -167,6 +175,7 @@ class MultiGPU(object):
 
     loss_list = []
     grads_list = []
+    accuracy = 0.0
 
     with tf.variable_scope(tf.get_variable_scope()):
       for gpu_idx, model in enumerate(self.models):
@@ -176,11 +185,13 @@ class MultiGPU(object):
           grads_and_vars = [(tf.clip_by_norm(g, self.max_grad_norm), v) for g, v in grads_and_vars]
           loss_list.append(loss)
           grads_list.append(grads_and_vars)
+          accuracy += model.accuracy
           tf.get_variable_scope().reuse_variables()
 
-    self.loss = tf.add_n(loss_list) / len(loss_list)
+    self.loss = tf.add_n(loss_list) / float(len(loss_list))
     self.grads_and_vars = self.average_gradients(grads_list)
     self.train_op = self.optimizer.apply_gradients(self.grads_and_vars, global_step=self.global_step)
+    self.accuracy = accuracy / len(self.models)
 
   def train(self, sess, images, labels):
     half_idx = len(images) // 2
@@ -189,7 +200,7 @@ class MultiGPU(object):
       images_feed = images[idx* half_idx: (idx+1)*half_idx]
       labels_feed = labels[idx* half_idx: (idx+1)*half_idx]
       feed_dict.update(model.build_feed_dict(images_feed, labels_feed, True))
-    return sess.run([self.train_op, self.loss, self.summary], feed_dict=feed_dict)
+    return sess.run([self.train_op, self.loss, self.accuracy, self.summary], feed_dict=feed_dict)
 
   def test(self, sess, input_x, sequence_length, input_y, keep_prob):
     return sess.run([self.loss, 
